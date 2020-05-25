556a75b7df47c309513c6e4821e74d5ea116b366
josephmannis <annis.jo@husky.neu.edu>
2020-05-24 21:52:01 -0400

    Fixes, get preview running.

--- index.jsx
+++ index.jsx
@@ -13,10 +13,10 @@ const PROXY = 'http://127.0.0.1:41417/';
  * initial state that gets passed on initial render of the widget.
  * before any command has been run.
  *
- * You can optionally delete this. If you delete it, it takes the form { output: '' }
+ * This is optional and can be deleted. If you delete it, it takes the form { output: '' }
  */
 export const initialState = {
-  output: 'Fetching...',
+  output: 'This is your example widget!',
   error: '',
 }
 
@@ -33,8 +33,8 @@ const eventCreator = {
  * 
  * Interact with this function by calling the dispatch function.
  * 
- * @param {type: string, data: any} event 
- * @param {any} previousState 
+ * @param { type: string, data: any } event 
+ * @param { any } previousState 
  */
 export const updateState = (event, previousState) => {
     switch (event.type) {
@@ -50,7 +50,9 @@ export const updateState = (event, previousState) => {
  * more advanced use cases. For example, instead of relying on periodic shell commands, you
  * might want to open and listen to WebSocket events to update your widget.
  * 
- * @param { ({type: string, data: any}) => void} dispatch: A function used to interact with updateState. Takes an event type and a data object.
+ * This function is optional and can be deleted.
+ * 
+ * @param { ({type: string, data: any}) => void } dispatch: A function used to interact with updateState. Takes an event type and a data object.
  */
 export const init = (dispatch) => {
     // dispatch({type: eventCreator.example, data: []});
@@ -63,7 +65,9 @@ export const init = (dispatch) => {
  * 
  * This is the first form. It receives the dispatch function and allows one to perform an action.
  * 
- * @param { ({type: string, data: any}) => void} dispatch: A function used to interact with updateState. Takes an event type and a data object.
+ * This is optional and can be deleted.
+ * 
+ * @param { ({type: string, data: any}) => void } dispatch: A function used to interact with updateState. Takes an event type and a data object.
  */
 export const command = async (dispatch) => {
   
@@ -81,36 +85,40 @@ export const refreshFrequency = 1.8e6; // 30m
 
 
 /**
- * An object or string defining the CSS rules to applied to the root of your widget.
+ * An object or string defining the CSS rules to applied to the root of your widget (your outermost element).
  * It is most commonly used control the position of your widget. 
- * It is converted to a CSS class name using the Emotion CSS-in-JS library.
+ * It is converted to a CSS class name using the Emotion CSS-in-JS library.\
+ * 
+ * This is optional and can be deleted.
  */
 export const className = `
-  right: 2em;
+  left: 2em;
   top: 2em;
   font-family: -apple-system;
   z-index: 1;
-  width: 20%;
+  width: 30%;
   max-height: 50%;
 `;
 
+
+const Widget = styled.div`
+  background-color: white;
+  border-radius: 14px;
+  padding: 4em;
+  text-align: center;
+`
 
 /**
  * If you know React functional components you know how render works. The props passed to 
  * this function is whatever state your updateState function returns. If you don't provide 
  * your own updateState function, the default props that are passed are output and error,
  * containing the output your command produced and any error that might have occurred. 
- * @param {any} state 
- * @param { ({type: string, data: any}) => void} dispatch: A function used to interact with updateState. Takes an event type and a data object.
+ * 
+ * @param { any } state: The current state
+ * @param { ({type: string, data: any}) => void } dispatch: A function used to interact with updateState. Takes an event type and a data object.
  */
 export const render = (state, dispatch) => (
-  error ? (
-    <div>
-      {`Error retrieving: ${error}`}
-    </div>
-  ) : (
-    <div>
-      { state.output }
-    </div>
-  )
+  <Widget>
+    { state.error ? `Error retrieving: ${state.error}` : state.output }
+  </Widget>
 );
